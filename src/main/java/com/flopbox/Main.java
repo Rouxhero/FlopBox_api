c04ce4b41ae337ecd30a7c360e58f0b8737caec2
Leo Le Van Canh dit Ban <leo.levancanhditban.etu@univ-lille.fr>
2023-03-31 21:57:55 +0200

    Correct URL

--- src/main/java/com/flopbox/Main.java
+++ src/main/java/com/flopbox/Main.java
@@ -15,8 +15,8 @@ import org.glassfish.jersey.server.ResourceConfig;
 public class Main {
 	// Base URI the Grizzly HTTP server will listen on
 	//
-	//public static final String BASE_URI = "http://api.givemecoffee.ninja:8080/";
-	static final String BASE_URI = "http://localhost:8080/";
+	public static final String BASE_URI = "http://api.givemecoffee.ninja:8080/";
+	//static final String BASE_URI = "http://localhost:8080/";
 
 
 	/**
