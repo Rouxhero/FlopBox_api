8bd8c352d917f7feccc09eb5b59f0c94361c962e
Leo Le Van Canh dit Ban <leo.levancanhditban.etu@univ-lille.fr>
2023-04-12 00:27:34 +0200

    Update Status

--- src/main/java/com/flopbox/resource/Status.java
+++ src/main/java/com/flopbox/resource/Status.java
@@ -9,6 +9,6 @@ public class Status {
 
     @GET
     public String status() {
-        return "<div style='background:#00000;color:#FFFFF'><h1>Welcome on flopbox api!</h1><br><h2>Server is running</h2></div>";
+        return "<div style=\"background: black;color: white;width: 100%;height: 100%;position: fixed;top: 0;left: 0;text-align: center;\"><h1>Welcome on flopbox api!</h1><br><h2>Server is running</h2></div>";
     }
 }
